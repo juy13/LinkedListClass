caf0189346748fc6d050a1bfea7e76866f1bd220
Julian <julyk.volyansky13@yandex.ru>
2019-10-11 20:16:20 +0300

    Add project files.

---
+++ LinkedList/LinkedList.cpp
@@ -0,0 +1,42 @@
+// LinkedList.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include "pch.h"
+#include "LinkedListClass.h"
+#include <iostream>
+
+
+
+int main()
+{
+	LinkedListClass<int> a;
+	a.addelem(3);
+	a.addelem(4);
+	a.addelem(7);
+	a.addelem(5);
+	a.printList();
+	a.deletelem(0);
+	std::cout << "IIIIIIIIIIIIIIIIIIIIIIIIII" << std::endl;
+	a.printList();
+	a.deletelem(0);
+	std::cout << "IIIIIIIIIIIIIIIIIIIIIIIIII" << std::endl;
+	a.printList();
+	a.deletelem(0);
+	std::cout << "IIIIIIIIIIIIIIIIIIIIIIIIII" << std::endl;
+	a.printList();
+	a.deletelem(0);
+	std::cout << "IIIIIIIIIIIIIIIIIIIIIIIIII" << std::endl;
+	a.printList();
+
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
---
+++ LinkedList/LinkedListClass.h
@@ -0,0 +1,123 @@
+﻿#pragma once
+#include <iostream>
+
+
+
+template<typename  TElement>
+struct list {
+	TElement field;
+	struct list<TElement> *ptr;
+};
+
+template<typename  TElement>
+class LinkedListClass
+{
+private:
+	list<TElement> *head;
+	int len = 0;
+	list<TElement> * Get(int i) const;
+	list<TElement> * Prev(list<TElement> * node) const;
+	bool isEmpty() const;
+
+public:
+	LinkedListClass();
+	~LinkedListClass();
+	
+	void addelem(TElement number);
+	void printList() const;
+	void deletelem(int ind);
+	//void deletehead();
+	//void listprint();
+};
+
+template<typename TElement>
+inline list<TElement>* LinkedListClass<TElement>::Get(int i) const
+{
+	list<TElement> *temp = head;
+	int ind = 0;
+	while (i != ind)
+	{
+		temp = temp->ptr;
+		ind++;
+	}
+	return temp;
+}
+
+template<typename TElement>
+inline list<TElement>* LinkedListClass<TElement>::Prev(list<TElement>* node) const
+{
+	if (isEmpty()) 
+		return NULL;
+	if (node == this->head) 
+		return NULL;
+	list<TElement> *p = head;
+	while (p->ptr != node)
+		p = p->ptr;
+	return p;
+}
+
+template<typename TElement>
+inline bool LinkedListClass<TElement>::isEmpty() const
+{
+	if (this->head == NULL)
+		return false;
+	else
+		return true;
+}
+
+template<typename TElement>
+LinkedListClass<TElement>::LinkedListClass()
+{
+	this->head = nullptr;
+}
+
+template<typename TElement>
+LinkedListClass<TElement>::~LinkedListClass()
+{
+}
+
+template<typename TElement>
+void LinkedListClass<TElement>::addelem(TElement number)
+{
+	list<TElement> *nd = new list<TElement>; //динамически создаем новый узел
+	nd->field = number;        //задаем узлу данные
+	nd->ptr = nullptr;     //новый узел в конце, поэтому NULL
+	if (this->head == nullptr)     //если создаем первый узел
+		this->head = nd;
+	else                 //если узел уже не первый
+	{
+		list<TElement> *current = head;
+		//ищем в цикле предшествующий последнему узел
+		while (current->ptr != nullptr)
+			current = current->ptr;
+		//предшествующий указывает на последний
+		current->ptr = nd;
+	}
+}
+
+template<typename TElement>
+void LinkedListClass<TElement>::printList() const
+{
+	list<TElement> *current = head;
+	while (current != nullptr)
+	{
+		std::cout << current->field << std::endl;
+		current = current->ptr;
+	}
+}
+
+template<typename TElement>
+inline void LinkedListClass<TElement>::deletelem(int ind)
+{
+	list<TElement> * node = this->Get(ind);
+	if (node == NULL) { return; } // В списке нет узлов
+	if (node == this->head)  // Удаление корневого узла
+	{
+		head = node->ptr;
+		delete node;
+		return;
+	}
+	list<TElement>* prev = this->Prev(node); // Удаление промежуточного узла
+	prev->ptr = node->ptr;
+	delete node;
+}
